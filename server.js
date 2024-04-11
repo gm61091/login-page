99fae3537d72061dab54c71f5ec11982c7c7ad8d
gary merriman <g.merriman691@gmail.com>
2024-04-11 12:47:27 -0500

    save

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const bcrypt = require('bcrypt');
 const PORT = 3000;
 const saltRounds = 8;
 
+const userTypedInPassword = 'mypassword';
+const storedPassword = '';
+
+bcrypt.hash(userTypedInPassword, saltRounds, (err, hash) => {
+    if (err) {
+        console.log(err);
+        return err
+    }
+    console.log('hashed password: ${hash}, user password: ${userTypedInPassword}');
+});
+
+bcrypt.compare(userTypedInPassword, storedPassword, (err, result) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    if (result) {
+        console.log('Passwords are a match');
+    }
+    else{
+        console.log("passwords don't match");
+    }
+});
+
+
 const app = express();
 app.use(express.json());
 
@@ -18,7 +43,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 8);
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
         // Store the user data in the database
         const newUser = await db.one(`
             INSERT INTO users (name, email, password) 
@@ -30,6 +55,7 @@ app.post('/register', async (req, res) => {
         res.status(500).send('Error registering user');
     }
 });
+
 
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/public/login.html');
@@ -59,3 +85,48 @@ app.post('/login', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}.`);
 });
+
+
+
+
+
+// const express = require(express);
+// const pgp = require(pg-promise)();
+// const db = pgp(postgres://qonnxkqn:yAamuth4AZ0bhZEGuoBLeR6tfHO-wXYC@raja.db.elephantsql.com/qonnxkqn);
+// const PORT = 3000;
+// const bcrypt = require('bcrypt')
+
+// const userTypedInPassword = 'mypassword';
+// const saltRounds = 8;
+// let storedPassword = ''
+
+// bcrypt.hash(userTypedInPassword, saltRounds, (err, hash) => {
+//     if (err) {
+//         console.log(err);
+//         return err
+//     }
+//     console.log(hashed password: ${hash}, user password: ${userTypedInPassword});
+// })
+
+
+// bcrypt.compare(userTypedInPassword, storedPassword, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     if (result) {
+//         console.log('Passwords are a match');
+//     }
+//     else{
+//         console.log("passwords don't match");
+//     }
+// })
+
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.static(client))
+
+// app.get('/connected', (req, res) => {
+//     res.sendFile(__dirname + '/client/index.html')
+// })
