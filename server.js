e7d579e7731f0113f19acc7f67d5cebdf744e29a
gary merriman <g.merriman691@gmail.com>
2024-04-11 11:51:01 -0500

    save

--- server.js
+++ server.js
@@ -4,24 +4,42 @@ const db = pgp(`postgres://qonnxkqn:yAamuth4AZ0bhZEGuoBLeR6tfHO-wXYC@raja.db.ele
 const PORT = 3000;
 const bcrypt = require('bcrypt');
 const saltRounds = 8;
-const myPlaintextPassword = 'waffles';
-const path = require('path');
+const userTypedInPassword = 'waffles';
+let storedPassword = '';
 
-bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-    });
+
+bcrypt.hash(userTypedInPassword, saltRounds, (err, hash) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    storedPassword = hash;
+    console.log(`Hashed password: ${hash}`);
 });
 
+// Compare hashed password with user input
+bcrypt.compare(userTypedInPassword, storedPassword, (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    if (result) {
+        console.log('Passwords match');
+    } else {
+        console.log("Passwords don't match");
+    }
+});
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'client' directory
+app.use(express.static('public'));
 
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/register.html');
+// Route to serve an HTML file
+app.get('/connected', (req, res) => {
+    res.sendFile(__dirname + '/public/register.html');
 });
-
 
 app.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 8);
@@ -35,8 +53,8 @@ app.post('/register', async (req, res) => {
     });
 
 
-    app.get('/login', (req, res) => {
-        res.sendFile(__dirname + '/login.html');
+app.get('/login', (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
     });
 
 
