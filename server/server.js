21d4f7d0517c63d448e576e23fed23ceb05529bf
gary merriman <g.merriman691@gmail.com>
2024-04-12 13:34:32 -0500

    save

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post('/register', async (req, res) => {
             INSERT INTO users (name, email, password) 
             VALUES ($1, $2, $3) 
             RETURNING id`, [name, email, hashedPassword]);
-        console.log('new registered user:', newUser);
+        console.log('new registered user');
         res.json('you are registered!');
 
     } catch (error) {
